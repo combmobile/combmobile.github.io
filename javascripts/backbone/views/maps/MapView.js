f02fc81c4b72d90cccd6b2e3b30cf3c23780ea7d
Crustacean87! <declanv@gmail.com>
2014-07-30 21:36:05 -0400

    Trying JQuery's .each on the pin list

--- javascripts/backbone/views/maps/MapView.js
+++ javascripts/backbone/views/maps/MapView.js
@@ -273,6 +273,7 @@ var mapStyles = [
 
     if  (typeof pins !== 'undefined') {
     _.each(pins, function (pin) {
+      self = this;
       console.log("pin iterator pins", pins);
       console.log("pin iterator this", this);
     var name = pin.name;
