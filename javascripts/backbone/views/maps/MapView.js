3ce63e9cdce8d874191fed0c8b59a4cc1c98f914
Crustacean87! <declanv@gmail.com>
2014-08-02 16:19:45 -0400

    Changed scope in the click listener

--- javascripts/backbone/views/maps/MapView.js
+++ javascripts/backbone/views/maps/MapView.js
@@ -325,6 +325,7 @@ function save_marker(Marker, mName, mAddress, mReplace, model, ib)
           name : mName, description : mAddress, photo_url : '', pin_lat : lat, pin_long : lng, map_id : model.id},
        {
         success: function(data) {
+          var self = this;
           console.log("pinmodel create data", data)
           pinData = data;
           var newContentString = $(
