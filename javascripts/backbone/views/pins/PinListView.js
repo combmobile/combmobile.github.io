4d9b44f4e75fe7f70fba080e4e6fee0c6d2af73f
Crustacean87! <declanv@gmail.com>
2014-08-02 13:04:43 -0400

    Attempting to selectively add events back in for the collection listener in the PinListView

--- javascripts/backbone/views/pins/PinListView.js
+++ javascripts/backbone/views/pins/PinListView.js
@@ -2,7 +2,7 @@ var Comb = Comb || { Models: {}, Collections: {}, Views: {} };
 
 Comb.Views.PinListView = Backbone.View.extend({
   initialize: function(){
-    // this.listenTo(this.collection, "all", this.render)
+    this.listenTo(this.collection, "change" "destroy" "add", this.render)
     console.log("you've created a pinListView")
   },
   render: function(){
