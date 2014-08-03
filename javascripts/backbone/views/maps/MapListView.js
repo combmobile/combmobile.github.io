3bcfe4413e7eb84f07da17cc8157d78b80d45afa
ChrisMLee <cristovao.m.lee@gmail.com>
2014-08-03 11:57:45 -0400

    testing add function

--- javascripts/backbone/views/maps/MapListView.js
+++ javascripts/backbone/views/maps/MapListView.js
@@ -20,5 +20,10 @@ Comb.Views.MapListView = Backbone.View.extend({
     var mapItemView = new Comb.Views.MapView( {model: map} )
     ul.prepend( mapItemView.renderMapList().el );
     })
-  }
+  },
+  close: function(){
+      this.remove();
+      this.unbind();
+      this.model.unbind("change", this.modelChanged);
+    }
 })
