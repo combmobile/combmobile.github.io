b2997a6ec1e1adf36018bed0d86095272a2634c3
Crustacean87! <declanv@gmail.com>
2014-08-08 12:05:50 -0400

    Removing the console.logs from the map and pin initialize functions:

--- javascripts/backbone/models/Map.js
+++ javascripts/backbone/models/Map.js
@@ -3,7 +3,6 @@ var Comb = Comb || { Models: {}, Collections: {}, Views: {} };
 Comb.Models.Map = Backbone.Model.extend({
   urlRoot : '/maps',
   initialize: function(){
-    console.log("You've created a new Map");
     // this.set('pins', new Comb.Collections.PinCollection());
   },
   defaults:{
--- javascripts/backbone/models/Pin.js
+++ javascripts/backbone/models/Pin.js
@@ -3,7 +3,6 @@ var Comb = Comb || { Models: {}, Collections: {}, Views: {} };
 Comb.Models.Pin = Backbone.Model.extend({
   urlRoot : '/pins',
   initialize: function(){
-    console.log("You've created a new Pin");
   },
   defaults:{
     name:'',
